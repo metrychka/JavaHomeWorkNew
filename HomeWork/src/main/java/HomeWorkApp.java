84f0c01de1cc4121d2f00f8931850e16e956e786
Anna Molodtsova <molodcova.ae@gmail.com>
2022-10-23 21:24:11 +0300

    Добавила класс и методы

---
+++ HomeWork/src/main/java/HomeWorkApp.java
@@ -0,0 +1,50 @@
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    public static void checkSumSign() {
+        int a = -20;
+        int b = 9;
+        int c;
+        c = a + b;
+
+        if (c >=0 ) {
+            System.out.println("Сумма положительная");
+        }
+        else  {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void printColor() {
+        int vaiue = 100;
+        if (vaiue <=0 ) {
+            System.out.println("Красный");
+        }
+        else if (vaiue > 0 && vaiue <= 100) {
+            System.out.println("Желтый");
+        }
+        else if (vaiue >100) {
+            System.out.println("Зеленый");
+        }
+    }
+    public static void compareNumbers() {
+        int a = 20;
+        int b = 78;
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+    }
+
+
+}
